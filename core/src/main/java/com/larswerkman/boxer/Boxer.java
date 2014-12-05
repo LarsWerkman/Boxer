734a5c9a592b4fd08c5df5edbe38d758f28167b3
Werkman, L - SPLXE <Lars.Werkman@klm.com>
2014-12-05 11:31:16 +0100

    Error handling boxer processor

--- core/src/main/java/com/larswerkman/boxer/Boxer.java
+++ core/src/main/java/com/larswerkman/boxer/Boxer.java
@@ -11,29 +11,29 @@ import java.util.List;
  */
 public abstract class Boxer {
 
-    protected Boxer(Object object){
-        //Empty constructor for wrapper class
+    public Boxer(Object object) {
+        //Ensure the wrapper classes have a default constructor
     }
 
-    public static Boxer from(Object object){
-        try{
-            if(Class.forName("android.os.Bundle")
-                    .isAssignableFrom(object.getClass())){
+    public static Boxer from(Object object) {
+        try {
+            if (Class.forName("android.os.Bundle")
+                    .isAssignableFrom(object.getClass())) {
                 return new BundleWrapper(object);
             }
-        } catch (ClassNotFoundException e){/*Do nothing*/}
-        try{
-            if(Class.forName("android.os.Parcel")
-                    .isAssignableFrom(object.getClass())){
+        } catch (ClassNotFoundException e) {/*Do nothing*/}
+        try {
+            if (Class.forName("android.os.Parcel")
+                    .isAssignableFrom(object.getClass())) {
                 return new ParcelWrapper(object);
             }
-        } catch (ClassNotFoundException e){/*Do nothing*/}
-        try{
-            if(Class.forName("com.google.android.gms.wearable.DataMap")
-                    .isAssignableFrom(object.getClass())){
+        } catch (ClassNotFoundException e) {/*Do nothing*/}
+        try {
+            if (Class.forName("com.google.android.gms.wearable.DataMap")
+                    .isAssignableFrom(object.getClass())) {
                 return new DataMapWrapper(object);
             }
-        } catch (ClassNotFoundException e){}
+        } catch (ClassNotFoundException e) {/*Do nothing*/}
         return null;
     }
 
@@ -41,93 +41,137 @@ public abstract class Boxer {
         Add methods
      */
     public abstract <T extends Boxable> void addBoxable(String key, T value);
+
     public abstract <T extends Boxable> void addBoxableList(String key, List<T> value);
+
     public abstract <T extends Boxable> void addBoxableArray(String key, T[] value);
 
     public abstract void addEnum(String key, Enum value);
+
     public abstract void addEnumArray(String key, Enum[] value);
+
     public abstract void addEnumList(String key, List<Enum> value);
 
     public abstract void addString(String key, String value);
+
     public abstract void addStringArray(String key, String[] value);
+
     public abstract void addStringList(String key, List<String> value);
 
     public abstract void addBoolean(String key, boolean value);
+
     public abstract void addBooleanArray(String key, boolean[] value);
+
     public abstract void addBooleanList(String key, List<Boolean> value);
 
     public abstract void addByte(String key, byte value);
+
     public abstract void addByteArray(String key, byte[] value);
+
     public abstract void addByteList(String key, List<Byte> value);
 
     public abstract void addChar(String key, char value);
+
     public abstract void addCharArray(String key, char[] value);
+
     public abstract void addCharList(String key, List<Character> value);
 
     public abstract void addShort(String key, short value);
+
     public abstract void addShortArray(String key, short[] value);
+
     public abstract void addShortList(String key, List<Short> value);
 
     public abstract void addInt(String key, int value);
+
     public abstract void addIntArray(String key, int[] value);
+
     public abstract void addIntList(String key, List<Integer> value);
 
     public abstract void addLong(String key, long value);
+
     public abstract void addLongArray(String key, long[] value);
+
     public abstract void addLongList(String key, List<Long> value);
 
     public abstract void addDouble(String key, double value);
+
     public abstract void addDoubleArray(String key, double[] value);
+
     public abstract void addDoubleList(String key, List<Double> value);
 
     public abstract void addFloat(String key, float value);
+
     public abstract void addFloatArray(String key, float[] value);
+
     public abstract void addFloatList(String key, List<Float> value);
 
     /*
         Get methods
      */
     public abstract <T extends Boxable> T getBoxable(String key, Class<T> clazz);
+
     public abstract <T extends Boxable> T[] getBoxableArray(String key, Class<T> clazz);
+
     public abstract <T extends Boxable> List<T> getBoxableList(String key, Class<T> clazz, Class<? extends List> listtype);
 
     public abstract <T extends Enum> T getEnum(String key, Class<T> clazz);
+
     public abstract <T extends Enum> T[] getEnumArray(String key, Class<T> clazz);
+
     public abstract <T extends Enum> List<T> getEnumList(String key, Class<T> clazz, Class<? extends List> listtype);
 
     public abstract String getString(String key);
+
     public abstract String[] getStringArray(String key);
+
     public abstract List<String> getStringList(String key, Class<? extends List> listtype);
 
     public abstract boolean getBoolean(String key);
+
     public abstract boolean[] getBooleanArray(String key);
+
     public abstract List<Boolean> getBooleanList(String key, Class<? extends List> listtype);
 
     public abstract byte getByte(String key);
+
     public abstract byte[] getByteArray(String key);
+
     public abstract List<Byte> getByteList(String key, Class<? extends List> listtype);
 
     public abstract char getChar(String key);
+
     public abstract char[] getCharArray(String key);
+
     public abstract List<Character> getCharList(String key, Class<? extends List> listtype);
 
     public abstract short getShort(String key);
+
     public abstract short[] getShortArray(String key);
+
     public abstract List<Short> getShortList(String key, Class<? extends List> listtype);
 
     public abstract int getInt(String key);
+
     public abstract int[] getIntArray(String key);
+
     public abstract List<Integer> getIntList(String key, Class<? extends List> listtype);
 
     public abstract long getLong(String key);
+
     public abstract long[] getLongArray(String key);
+
     public abstract List<Long> getLongList(String key, Class<? extends List> listtype);
 
     public abstract double getDouble(String key);
+
     public abstract double[] getDoubleArray(String key);
+
     public abstract List<Double> getDoubleList(String key, Class<? extends List> listtype);
 
     public abstract float getFloat(String key);
+
     public abstract float[] getFloatArray(String key);
+
     public abstract List<Float> getFloatList(String key, Class<? extends List> listtype);
 }
--- core/src/main/java/com/larswerkman/boxer/internal/BoxerProcessor.java
+++ core/src/main/java/com/larswerkman/boxer/internal/BoxerProcessor.java
@@ -6,10 +6,12 @@ import com.larswerkman.boxer.annotations.Packet;
 import com.larswerkman.boxer.annotations.Wrap;
 import com.squareup.javawriter.JavaWriter;
 
+import javax.annotation.Generated;
 import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.*;
 import javax.lang.model.type.*;
+import javax.lang.model.util.ElementFilter;
 import javax.lang.model.util.Elements;
 import javax.lang.model.util.Types;
 import javax.tools.Diagnostic;
@@ -20,7 +22,7 @@ import java.util.*;
 /**
  * Created by lars on 13-11-14.
  */
-@SupportedSourceVersion(SourceVersion.RELEASE_6)
+@SupportedSourceVersion(SourceVersion.RELEASE_8)
 @SupportedAnnotationTypes("com.larswerkman.boxer.annotations.Box")
 public class BoxerProcessor extends AbstractProcessor {
 
@@ -55,25 +57,74 @@ public class BoxerProcessor extends AbstractProcessor {
         Set<? extends Element> elements = env.getElementsAnnotatedWith(Box.class);
         for (Element element : elements) {
             TypeElement typeElement = (TypeElement) element;
-            //Check if implements Boxable interface
+            //Check if implements a Boxable interface
             if (typeUtils.isAssignable(typeElement.asType(), TYPE_BOXABLE)) {
+
+                //Check if the class has a usable public/no-args constructor
+                boolean usableConstructor = false;
+                List<ExecutableElement> constructors = ElementFilter.constructorsIn(typeElement.getEnclosedElements());
+                for (ExecutableElement constructor : constructors) {
+                    if (constructor.getParameters().size() == 0) {
+                        usableConstructor = true;
+                        break;
+                    }
+                }
+                if (!usableConstructor) {
+                    log.printMessage(Diagnostic.Kind.ERROR,
+                            String.format("%s class should have a public constructor with no-args", typeElement.getSimpleName())
+                    );
+                    return true;
+                }
+
                 List<PackedField> fields = new ArrayList<PackedField>();
                 List<? extends Element> enclosedElements = typeElement.getEnclosedElements();
                 for (Element child : enclosedElements) {
+
+                    //Check if the child element has a Packet Annotation
                     Packet annotation = child.getAnnotation(Packet.class);
                     if (annotation != null) {
                         String name = child.getSimpleName().toString();
+
+                        //Find out of the child element is accessible
                         Modifier modifier = null;
                         for (Modifier mod : child.getModifiers()) {
-                            if (mod == Modifier.PUBLIC || mod == Modifier.PRIVATE || mod == Modifier.PROTECTED) {
+                            if (mod == Modifier.PUBLIC) {
+                                modifier = mod;
+                            } else if (mod == Modifier.PRIVATE || mod == Modifier.PROTECTED) {
+                                boolean getter = false;
+                                boolean setter = false;
+                                List<ExecutableElement> methods = ElementFilter.methodsIn(typeElement.getEnclosedElements());
+                                for (ExecutableElement method : methods) {
+                                    if (method.getSimpleName().contentEquals(String.format("get%s", capitalize(name)))
+                                            && method.getParameters().size() == 0
+                                            && typeUtils.isAssignable(method.getReturnType(), child.asType())) {
+                                        getter = true;
+                                        continue;
+                                    }
+
+                                    if (method.getSimpleName().contentEquals(String.format("set%s", capitalize(name)))
+                                            && method.getParameters().size() == 1
+                                            && typeUtils.isAssignable(method.getParameters().get(0).asType(), child.asType())) {
+                                        setter = true;
+                                    }
+                                }
+                                if (!getter || !setter) {
+                                    log.printMessage(Diagnostic.Kind.ERROR,
+                                            String.format("%s @Packet fied have default getter and setter methods or should be public",
+                                                    child.getSimpleName()));
+                                    return true;
+                                }
                                 modifier = mod;
                             } else if (mod == Modifier.FINAL) {
                                 log.printMessage(Diagnostic.Kind.ERROR,
-                                        "Packet annotation can't be placed on final types");
+                                        String.format("%s @Packet fied cannot be final",
+                                                child.getSimpleName())
+                                );
                                 return true;
                             }
                         }
 
+                        //Check if @Packet annotated field as has @Wrap annotation
                         Wrap wrap = child.getAnnotation(Wrap.class);
                         TypeMirror wrapType = null;
                         if (wrap != null) {
@@ -81,10 +132,18 @@ public class BoxerProcessor extends AbstractProcessor {
                                 wrap.value();
                             } catch (MirroredTypeException e) {
                                 wrapType = e.getTypeMirror();
+                                if (!typeUtils.isAssignable(wrapType, child.asType())) {
+                                    log.printMessage(Diagnostic.Kind.ERROR,
+                                            String.format("%s @Wrap annotated value %s is not assignable from %s",
+                                                    child.getSimpleName(), wrapType.toString(), child.asType().toString())
+                                    );
+                                    return true;
+                                }
                             }
                         }
 
-                        TypeMirror type = ((VariableElement) child).asType();
+                        //Check if the field type is acceptable
+                        TypeMirror type = child.asType();
                         if (isAcceptable(type) || isEnum(type)) {
                             fields.add(new PackedField(name, type, modifier, false, wrapType));
                         } else if (isArray(type)) {
@@ -93,15 +152,27 @@ public class BoxerProcessor extends AbstractProcessor {
                                 if (isAcceptable(arrayType)) {
                                     fields.add(new PackedField(name, type, modifier, true, wrapType));
                                 } else {
+                                    log.printMessage(Diagnostic.Kind.ERROR,
+                                            String.format("%s @Packed annotation can only be placed on arrays with type of" +
+                                                            ": primitives and wrappers, Enum classes and objects " +
+                                                            "implementing the boxable interface with @Box annotation",
+                                                    child.getSimpleName())
+                                    );
                                     log.printMessage(Diagnostic.Kind.ERROR, arrayType.toString());
                                 }
                             } else {
-                                log.printMessage(Diagnostic.Kind.ERROR, type.toString());
+                                log.printMessage(Diagnostic.Kind.ERROR,
+                                        String.format("%s @Packed annotated array should have an specified type",
+                                                child.getSimpleName())
+                                );
+                                return true;
                             }
                         } else {
                             log.printMessage(Diagnostic.Kind.ERROR,
-                                    "Packet annotation can only be placed on primitives " +
-                                            "or one objects the are assignable from the Boxable interface");
+                                    String.format("%s @Packed annotation can only be placed on: primitives and wrappers," +
+                                                    "Enum classes and objects implementing the boxable interface with @Box annotation",
+                                            child.getSimpleName())
+                            );
                             return true;
                         }
                     }
@@ -110,6 +181,12 @@ public class BoxerProcessor extends AbstractProcessor {
                 if (fields.size() > 0) {
                     brewJava(typeElement, fields);
                 }
+            } else {
+                log.printMessage(Diagnostic.Kind.ERROR,
+                        String.format("%s @Box annotated class should always implement the Boxable interface",
+                                element.getSimpleName())
+                );
+                return true;
             }
         }
         return true;
@@ -126,6 +203,7 @@ public class BoxerProcessor extends AbstractProcessor {
             JavaWriter writer = new JavaWriter(jfo.openWriter());
             writer.emitPackage(getPackage(classElement))
                     .emitImports(Boxer.class.getName(), List.class.getName(), ArrayList.class.getName(), originalQualified)
+                    .emitAnnotation(Generated.class, JavaWriter.stringLiteral(getClass().getCanonicalName()))
                     .beginType(simple, "class", EnumSet.of(Modifier.PUBLIC, Modifier.FINAL))
                     .beginMethod("void", METHOD_WRITE, EnumSet.of(Modifier.PUBLIC, Modifier.STATIC), original, "boxable", "Boxer", "boxer");
             for (PackedField field : fields) {
@@ -134,13 +212,13 @@ public class BoxerProcessor extends AbstractProcessor {
                 if (!field.isArray()) {
 
                     //Check what type the field is
-                    if(isPrimitiveOrWrapper(field.type())){
+                    if (isPrimitiveOrWrapper(field.type())) {
                         writer.emitStatement("boxer.add%s(\"%s\", boxable.%s)",
                                 unboxedName(field.type()), field.name(), field.getter()
                         );
-                    } else if(isString(field.type())){
+                    } else if (isString(field.type())) {
                         writer.emitStatement("boxer.addString(\"%s\", boxable.%s)", field.name(), field.getter());
-                    } else if(isEnum(field.type())){
+                    } else if (isEnum(field.type())) {
                         writer.emitStatement("boxer.addEnum(\"%s\", boxable.%s)", field.name(), field.getter());
                     } else {
                         writer.emitStatement("boxer.addBoxable(\"%s\", boxable.%s)", field.name(), field.getter());
@@ -149,13 +227,13 @@ public class BoxerProcessor extends AbstractProcessor {
 
                     //Check if type if is [] or List and use appropriete signature
                     String signature = field.type().getKind() == TypeKind.ARRAY ? "Array" : "List";
-                    if(isPrimitiveOrWrapper(field.type())){
+                    if (isPrimitiveOrWrapper(field.type())) {
                         writer.emitStatement("boxer.add%s%s(\"%s\", boxable.%s)",
                                 unboxedName(field.type()), signature, field.name(), field.getter()
                         );
-                    } else if(isString(field.type())){
+                    } else if (isString(field.type())) {
                         writer.emitStatement("boxer.addString%s(\"%s\", boxable.%s)", signature, field.name(), field.getter());
-                    } else if(isEnum(field.type())){
+                    } else if (isEnum(field.type())) {
                         writer.emitStatement("boxer.addEnum%s(\"%s\", boxable.%s)", signature, field.name(), field.getter());
                     } else {
                         writer.emitStatement("boxer.addBoxable%s(\"%s\", boxable.%s)", signature, field.name(), field.getter());
@@ -174,15 +252,15 @@ public class BoxerProcessor extends AbstractProcessor {
                 if (!field.isArray()) {
 
                     //Check what type the field is
-                    if(isPrimitiveOrWrapper(field.type())){
+                    if (isPrimitiveOrWrapper(field.type())) {
                         writer.emitStatement("boxable.%s", field.setter(
                                         String.format("boxer.get%s(\"%s\")", unboxedName(field.type()), field.name()))
                         );
-                    } else if(isString(field.type())){
+                    } else if (isString(field.type())) {
                         writer.emitStatement("boxable.%s", field.setter(
                                         String.format("boxer.getString(\"%s\")", field.name()))
                         );
-                    } else if(isEnum(field.type())){
+                    } else if (isEnum(field.type())) {
                         writer.emitStatement("boxable.%s", field.setter(
                                         String.format("boxer.getEnum(\"%s\", %s.class)", field.name(), field.type()))
                         );
@@ -201,15 +279,15 @@ public class BoxerProcessor extends AbstractProcessor {
                         if (field.type().getKind() == TypeKind.ARRAY) {
 
                             //Check what type the field is
-                            if(isPrimitiveOrWrapper(arrayType)){
+                            if (isPrimitiveOrWrapper(arrayType)) {
                                 writer.emitStatement("boxable.%s", field.setter(
                                                 String.format("boxer.get%sArray(\"%s\")", unboxedName(field.type()), field.name()))
                                 );
-                            } else if(isString(arrayType)) {
+                            } else if (isString(arrayType)) {
                                 writer.emitStatement("boxable.%s", field.setter(
                                                 String.format("boxer.getStringArray(\"%s\")", field.name()))
                                 );
-                            } else if(isEnum(arrayType)){
+                            } else if (isEnum(arrayType)) {
                                 writer.emitStatement("boxable.%s", field.setter(
                                                 String.format("boxer.getEnumArray(\"%s\", %s.class)", field.name(), arrayType))
                                 );
@@ -226,8 +304,6 @@ public class BoxerProcessor extends AbstractProcessor {
                                     elementUtils.getTypeElement("java.util.List"), arrayType
                             );
                             if (typeUtils.isSameType(declaredListType, field.type())) {
-
-                                //Check if a @Wrap interface has been declared, TODO check if its really a subtype of List
                                 if (field.wrapper() != null) {
                                     listtype = field.wrapper().toString();
                                 } else {
@@ -236,17 +312,17 @@ public class BoxerProcessor extends AbstractProcessor {
                             }
 
                             //Check what type the field is
-                            if(isPrimitiveOrWrapper(arrayType)){
+                            if (isPrimitiveOrWrapper(arrayType)) {
                                 writer.emitStatement("boxable.%s", field.setter(
                                                 String.format("boxer.get%sList(\"%s\", %s.class)",
                                                         unboxedName(field.type()), field.name(), listtype))
                                 );
-                            } else if(isString(arrayType)){
+                            } else if (isString(arrayType)) {
                                 writer.emitStatement("boxable.%s", field.setter(
                                                 String.format("boxer.getStringList(\"%s\", %s.class)",
                                                         field.name(), listtype))
                                 );
-                            } else if(isEnum(arrayType)){
+                            } else if (isEnum(arrayType)) {
                                 writer.emitStatement("boxable.%s", field.setter(
                                                 String.format("boxer.getEnumList(\"%s\", %s.class, %s.class)",
                                                         field.name(), arrayType, listtype))
@@ -277,7 +353,7 @@ public class BoxerProcessor extends AbstractProcessor {
     }
 
     private String unboxedName(TypeMirror type) {
-        if(type.getKind().isPrimitive()){
+        if (type.getKind().isPrimitive()) {
             return capitalize(type.toString());
         }
         return capitalize(typeUtils.unboxedType(type).toString());
@@ -295,7 +371,7 @@ public class BoxerProcessor extends AbstractProcessor {
                 || type.toString().equals("java.lang.Boolean"));
     }
 
-    public boolean isString(TypeMirror type){
+    public boolean isString(TypeMirror type) {
         return typeUtils.isSameType(type, TYPE_STRING);
     }
 
@@ -325,7 +401,7 @@ public class BoxerProcessor extends AbstractProcessor {
         }
     }
 
-    private String capitalize(String string){
+    private String capitalize(String string) {
         return string.substring(0, 1).toUpperCase() + string.substring(1);
     }
 }
